e104cb2e80d63442ff60c64976a68fa54613061f
VastyJoy <llex.tihonov@gmail.com>
2019-04-10 22:02:27 +0300

    123

--- src/main/java/lession13/ExecutionManager/TaskExecutionManager.java
+++ src/main/java/lession13/ExecutionManager/TaskExecutionManager.java
@@ -3,26 +3,26 @@ package lession13.ExecutionManager;
 import java.util.concurrent.CountDownLatch;
 
 public class TaskExecutionManager implements Runnable {
-   private final ContextImpl context;
+    private final ContextImpl context;
     private volatile Runnable runnable;
     private volatile boolean isCallBack;
     private final CountDownLatch barier;
 
     TaskExecutionManager(ContextImpl context, Runnable runnable, CountDownLatch barier, boolean isCallBack) {
-        this.runnable=runnable;
-        this.barier=barier;
+        this.runnable = runnable;
+        this.barier = barier;
         this.context = context;
-        this.isCallBack=isCallBack;
+        this.isCallBack = isCallBack;
     }
 
     @Override
     public void run() {
 
         try {
-            if(context.getContextInterrupt().get() || Thread.currentThread().isInterrupted()){
-                throw  new InterruptedException();
+            if (context.getContextInterrupt().get() || Thread.currentThread().isInterrupted()) {
+                throw new InterruptedException();
             }
-            if(isCallBack){
+            if (isCallBack) {
                 barier.await();
                 context.setIsFinished(true);
             }
@@ -30,14 +30,13 @@ public class TaskExecutionManager implements Runnable {
             context.incrementCompletedTaskCount();
             barier.countDown();
 
-        }catch (InterruptedException x) {
+        } catch (InterruptedException x) {
             context.incrementInterruptedTaskCount();
             barier.countDown();
-        }
-        catch (Exception x){
+        } catch (Exception x) {
             x.printStackTrace();
-        context.incrementFailedTaskCount();
-        barier.countDown();
+            context.incrementFailedTaskCount();
+            barier.countDown();
         }
 
     }
